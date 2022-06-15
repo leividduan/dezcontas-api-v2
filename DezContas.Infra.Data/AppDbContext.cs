e61e40ee922745970b4bbbb094d7a0391f04ca34
Deivid <deivid.cardos@gmail.com>
2022-06-15 00:08:07 -0300

    implement: DbContext with some overrides

---
+++ DezContas.Infra.Data/AppDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DezContas.Infra.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options)
+			: base(options) { }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+				e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+				property.SetColumnType("varchar(100)");
+
+			modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+			var cascadeFKs = modelBuilder.Model.GetEntityTypes()
+					.SelectMany(t => t.GetForeignKeys())
+					.Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Cascade);
+
+			foreach (var fk in cascadeFKs)
+				fk.DeleteBehavior = DeleteBehavior.Restrict;
+		}
+
+		public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+		{
+			foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("CreatedAt") != null))
+			{
+				if (entry.State == EntityState.Added)
+				{
+					entry.Property("CreatedAt").CurrentValue = DateTime.Now;
+					entry.Property("UpdatedAt").CurrentValue = DateTime.Now;
+				}
+
+				if (entry.State == EntityState.Modified)
+				{
+					entry.Property("CreatedAt").IsModified = false;
+					entry.Property("UpdatedAt").CurrentValue = DateTime.Now;
+				}
+			}
+			return base.SaveChangesAsync();
+		}
+	}
+}
