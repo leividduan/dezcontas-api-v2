89d8bd6de776212a2fdb02ad262fea0466e27c5d
Deivid <deivid.cardos@gmail.com>
2022-06-16 11:16:39 -0300

    implement: ErrorViewModel

---
+++ DezContas.API/ViewModel/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PlayPedidos.API.ViewModel
+{
+	public record ErrorViewModel(
+		List<ErrorDetailsViewModel> Errors
+	);
+
+	public record ErrorDetailsViewModel(
+		string Field,
+		List<string> Messages
+	);
+}
