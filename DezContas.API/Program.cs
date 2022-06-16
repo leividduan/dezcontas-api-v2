a3615b90e569103c8d31361f8b37969b965de7de
Deivid <deivid.cardos@gmail.com>
2022-06-16 11:16:56 -0300

    build: add AutoMapper configuration into project

--- DezContas.API/Program.cs
+++ DezContas.API/Program.cs
@@ -1,8 +1,25 @@
+using AutoMapper;
+using DezContas.Infra.IoC;
+using PlayPedidos.API.DTOs.Mappings;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+#region [AutoMapper]
+
+var mappingConfig = new MapperConfiguration(mc =>
+{
+	mc.AddProfile(new MappingProfile());
+});
+IMapper mapper = mappingConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+#endregion
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ DezContas.API/ViewModel/Mappings/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using DezContas.Domain.Entities;
+using PlayPedidos.API.ViewModel;
+
+namespace PlayPedidos.API.DTOs.Mappings
+{
+	public class MappingProfile : Profile
+	{
+		public MappingProfile()
+		{
+			CreateMap<Error, ErrorViewModel>().ReverseMap();
+			CreateMap<ErrorDetails, ErrorDetailsViewModel>().ReverseMap();
+		}
+	}
+}
