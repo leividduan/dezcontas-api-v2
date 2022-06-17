1a9d6faff810623396ad44cecdbd4ba0cbbaa71e
Deivid <deivid.cardos@gmail.com>
2022-06-17 10:58:01 -0300

    implement: UserController and AuthController

---
+++ DezContas.API/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using DezContas.API.ViewModel;
+using DezContas.Application.Interfaces;
+using DezContas.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using PlayPedidos.API.ViewModel;
+
+namespace DezContas.API.Controllers
+{
+	[Route("api/v1/auth")]
+	[ApiController]
+	public class AuthController : ControllerBase
+	{
+		private readonly IMapper _mapper;
+		private readonly IUserService _userService;
+
+		public AuthController(IMapper mapper, IUserService userService)
+		{
+			_mapper = mapper;
+			_userService = userService;
+		}
+
+		[Route("register")]
+		[HttpPost]
+		public async Task<IActionResult> Register([FromBody] UserPostViewModel userViewModel)
+		{
+			var user = _mapper.Map<User>(userViewModel);
+
+			if (user == null)
+				return BadRequest();
+
+			if (!user.IsValid())
+				return BadRequest(_mapper.Map<ErrorViewModel>(user.GetErrors()));
+
+			user.Password = user.HashPassword(user.Password);
+
+			await _userService.Add(user);
+
+			var newUserViewModel = _mapper.Map<UserViewModel>(user);
+			return RedirectToAction("GetById", "User", new { id = newUserViewModel.Id });
+		}
+	}
+}
---
+++ DezContas.API/Controllers/UserController.cs
@@ -0,0 +1,77 @@
+﻿using AutoMapper;
+using DezContas.API.ViewModel;
+using DezContas.Application.Interfaces;
+using DezContas.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using PlayPedidos.API.ViewModel;
+
+namespace DezContas.API.Controllers
+{
+	[Route("api/v1/user")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		private readonly IMapper _mapper;
+		private readonly IUserService _userService;
+
+		public UserController(IMapper mapper, IUserService userService)
+		{
+			_mapper = mapper;
+			_userService = userService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAll()
+		{
+			var users = await _userService.Get();
+
+			var usersViewModel = _mapper.Map<IEnumerable<UserViewModel>>(users);
+
+			return Ok(usersViewModel);
+		}
+
+		[HttpGet("{id}", Name = nameof(GetById))]
+		public async Task<IActionResult> GetById(Guid id)
+		{
+			var user = await _userService.GetSingle(x => x.Id == id);
+
+			var viewModel = _mapper.Map<UserViewModel>(user);
+
+			if (viewModel == null)
+				NotFound();
+
+			return Ok(viewModel);
+		}
+
+		[HttpPut("{id}")]
+		[ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Put))]
+		public async Task<IActionResult> PutById(Guid id, [FromBody] UserPutViewModel userViewModel)
+		{
+			if (id != userViewModel.Id)
+				return BadRequest();
+
+			var user = _mapper.Map<User>(userViewModel);
+
+			if (!user.IsValid())
+				return BadRequest(_mapper.Map<ErrorViewModel>(user.GetErrors()));
+
+			await _userService.Edit(user);
+
+			var editedUserViewModel = _mapper.Map<UserViewModel>(user);
+			return Ok(editedUserViewModel);
+		}
+
+		[HttpDelete]
+		public async Task<IActionResult> DeleteById(Guid id)
+		{
+			var user = await _userService.GetSingle(x => x.Id == id);
+
+			if (user == null)
+				return BadRequest();
+
+			await _userService.Delete(user);
+
+			return NoContent();
+		}
+	}
+}
