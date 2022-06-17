8212c10259694e2a13fdebfee963003e48dff9e1
Deivid <deivid.cardos@gmail.com>
2022-06-17 11:13:43 -0300

    migration: reneame a User property

---
+++ DezContas.Infra.Data/Migrations/20220617141207_renameUserProperty.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DezContas.Infra.Data.Migrations
+{
+    public partial class renameUserProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AtSign",
+                table: "User",
+                newName: "Username");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_User_AtSign",
+                table: "User",
+                newName: "IX_User_Username");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "User",
+                type: "varchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(200)",
+                oldMaxLength: 200)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "User",
+                newName: "AtSign");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_User_Username",
+                table: "User",
+                newName: "IX_User_AtSign");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "User",
+                type: "varchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(500)",
+                oldMaxLength: 500)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
