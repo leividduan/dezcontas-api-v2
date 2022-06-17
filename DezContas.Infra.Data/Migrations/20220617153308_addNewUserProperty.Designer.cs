e8bf496e1c37782b075c39b433c24576a6920844
Deivid <deivid.cardos@gmail.com>
2022-06-17 12:33:41 -0300

    migration: add new User property

--- DezContas.Infra.Data/Migrations/20220617153308_addNewUserProperty.Designer.cs
+++ DezContas.Infra.Data/Migrations/20220617153308_addNewUserProperty.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DezContas.Infra.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DezContas.Infra.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220617153308_addNewUserProperty")]
+    partial class addNewUserProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DezContas.Infra.Data/Migrations/20220617153308_addNewUserProperty.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DezContas.Infra.Data.Migrations
+{
+    public partial class addNewUserProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "User",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "User");
+        }
+    }
+}
